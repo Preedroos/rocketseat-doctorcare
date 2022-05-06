28895b02ed7148d36968b33de76a6c6da762cbd4
Preedroos <preedroos@gmail.com>
2022-05-06 00:39:30 -0400

    feat: adding menu-expanded

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function onScroll() {
   if (scrollY > 0) {
-    navigation.classList.add('scroll');
+    navigation.classList.add('scroll')
   } else {
-    navigation.classList.remove('scroll');
-  }	
+    navigation.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  if (!document.body.classList.contains('menu-expanded')) {
+    document.body.classList.add('menu-expanded')
+    navigation.classList.add('scroll')
+  }
+}
+
+function closeMenu() {
+  if (document.body.classList.contains('menu-expanded')) {
+    document.body.classList.remove('menu-expanded')
+    navigation.classList.remove('scroll')
+  }
 }
