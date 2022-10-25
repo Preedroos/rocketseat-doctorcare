716f0147503f1da1cee663d85c7206c8d039b415
Preedroos <preedroos@gmail.com>
2022-10-25 11:57:51 -0400

    fix: showAltNav(), showUpButton() and html style

--- scripts.js
+++ scripts.js
@@ -1,78 +1,78 @@
 //randomColor()
 
 function randomColor() {
-  color = Math.ceil(Math.random() * 10) * 100
-  document.documentElement.style.setProperty('--hue', color)
-  console.log(color)
+  color = Math.ceil(Math.random() * 10) * 100;
+  document.documentElement.style.setProperty('--hue', color);
+  console.log(color);
 }
 
 function onScroll() {
-  showAltNav()
-  showUpButton()
+  showAltNav();
+  showUpButton();
 }
 
 function openMenu() {
   if (!document.body.classList.contains('menu-expanded')) {
-    document.body.classList.add('menu-expanded')
+    document.body.classList.add('menu-expanded');
   }
 }
 
 function closeMenu() {
   if (document.body.classList.contains('menu-expanded')) {
-    document.body.classList.remove('menu-expanded')
+    document.body.classList.remove('menu-expanded');
   }
 }
 
 function showAltNav() {
   if (scrollY > 0) {
-    navigation.classList.add('scroll')
+    document.getElementById('navigation').classList.add('scroll');
   } else {
-    navigation.classList.remove('scroll')
+    document.getElementById('navigation').classList.remove('scroll');
   }
 }
 
 function showUpButton() {
   if (scrollY > 1000) {
-    upButton.classList.add('show')
+    document.getElementById('upButton').classList.add('show');
   } else {
-    upButton.classList.remove('show')
+    document.getElementById('upButton').classList.remove('show');
   }
 }
 
 ScrollReveal({
   origin: 'top',
   distance: '50px',
-  duration: 1500
+  duration: 1500,
 }).reveal(`
 .stat,
-.card`)
+.card`);
 
 ScrollReveal({
   origin: 'bottom',
   distance: '50px',
-  duration: 1500
+  duration: 1500,
 }).reveal(`
 #home .button,
-#contact .button`)
+#contact .button`);
 
 ScrollReveal({
   origin: 'left',
   distance: '50px',
-  duration: 500
-}).reveal(`header h1`)
+  duration: 500,
+}).reveal(`header h1`);
 
 ScrollReveal({
   origin: 'left',
   distance: '50px',
-  duration: 1000
+  duration: 1000,
 }).reveal(`
 header h2,
-header h3`)
+header h3`);
 
 ScrollReveal({
   origin: 'left',
   distance: '50px',
-  duration: 1500
+  duration: 1500,
 }).reveal(`
 header > p,
-#about p`)
+#about p`);
